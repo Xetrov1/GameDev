174b0f65d3765d0d9d059647e3872d629269b4ef
Unknown <chandler.g484@stu.nebo.edu>
2018-09-14 10:27:16 -0600

    Grapplehook/morePlatforms

---
+++ 2d project/Assets/Scripts/Grapple.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        double momentum, speed = 5, range = 15;
+        momentum = speed + GetComponent<Rigidbody2D>().velocity.x;
+        bool g = false;
+        if (g == true && Input.GetKey(KeyCode.Q)) {
+            
+        }
+
+	}
+}
