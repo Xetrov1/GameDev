6c257d0f20118e64bc5541ca707e2d45dbdf9dfd
Unknown <chandler.g484@stu.nebo.edu>
2018-10-10 10:28:28 -0600

    added shooting
    fixed bugs

---
+++ 2d project/Assets/Scripts/Pew.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pew : MonoBehaviour {
+
+    public float Speed;
+
+    public Rigidbody2D pc;
+
+    public GameObject EnemyDeath;
+
+    public GameObject ProjectileParticle;
+    public int PointsforKill;
+
+	// Use this for initialization
+	void Start () {
+        // Player = FindObjectOfType<Rigidbody2D>();
+        if (pc.transform.localScale.x < 0)
+            Speed = -Speed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Speed, GetComponent<Rigidbody2D>().velocity.y);
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+        if(other.tag == "Enemy"){
+            Instantiate(EnemyDeath, other.transform.position, other.transform.rotation);
+            Destroy(other.gameObject);
+            Scoremanager.AddPoints(PointsforKill);
+        } // ends if
+        Instantiate(ProjectileParticle, transform.position, transform.rotation);
+        Destroy(gameObject);
+    } // ends ontrigger 
+}
---
+++ 2d project/Assets/Scripts/Shoot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+
+    public Transform firePoint;
+
+    public GameObject projectile;
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Mouse0)) {
+            Instantiate(projectile, firePoint.position, firePoint.rotation);
+        }
+	}
+}
