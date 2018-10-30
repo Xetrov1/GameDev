e5c888df7892d16e862b0465ce940692ed7f15ae
Unknown <chandler.g484@stu.nebo.edu>
2018-10-30 10:27:01 -0600

    Added CameraFollowScript
    MAGIC ITS SO MAGICAL AND POTATOES

---
+++ 2d project/Assets/Scripts/SmoothFollowCamera.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollowCamera : MonoBehaviour {
+
+    public bool isFollowing;
+    public CharMove pc;
+    // camera position offset
+    public float xOffset;
+
+    public float yOffset;
+	// Use this for initialization
+	void Start () {
+        pc = FindObjectOfType<CharMove>();
+
+        isFollowing = true;
+	}
+     
+	// Update is called once per frame
+	void Update () {
+        if(isFollowing) {
+            transform.position = new Vector3(pc.transform.position.x + xOffset, pc.transform.position.y + yOffset, transform.position.z);
+        }
+	}
+}
