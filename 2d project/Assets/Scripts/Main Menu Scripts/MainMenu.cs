ee5dd9557adb2485b0d9227928285e8c54990ca9
Unknown <chandler.g484@stu.nebo.edu>
2018-11-28 10:29:07 -0700

    Fixed more bugs should be working
    Added Main Menu

---
+++ 2d project/Assets/Scripts/Main Menu Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public int leveltoLoad;
+
+    public void LoadLevel() {
+        SceneManager.LoadScene(leveltoLoad);
+    }
+
+    public void LevelExit() {
+        Application.Quit();
+    }
+}
